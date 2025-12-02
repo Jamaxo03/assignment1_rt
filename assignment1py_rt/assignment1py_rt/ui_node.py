1720e2f5fa0544bff7dc821b9eea3940c9e5eaad
Jamaxo03 <mattecrova03@gmail.com>
2025-12-02 10:52:38 +0100

    Update UI_node
    wait_for_two_turtles() and choose_turtle() added

--- assignment1py_rt/assignment1py_rt/ui_node.py
+++ assignment1py_rt/assignment1py_rt/ui_node.py
@@ -8,22 +8,61 @@ class UINode(Node):
     def __init__(self):
         super().__init__('ui_node')
 
-        # publisher
-        self.pub_t1 = self.create_publisher(Twist, '/turtle1/cmd_vel', 10)
+        self.wait_for_two_turtles()
 
-        self.get_logger().info("UI node started")
-        self.user_loop()   
+        # publishers
+        self.pub_t1 = self.create_publisher(Twist, '/turtle1/cmd_vel', 10)
+        self.pub_t2 = self.create_publisher(Twist, '/turtle2/cmd_vel', 10)
+
+        self.selected_pub = self.choose_turtle()
+
+        self.user_loop()  
+
+    def wait_for_two_turtles(self):
+        
+        self.get_logger().info("Checking turtle1 and turtle2")
+
+        while True:
+            topic_list = self.get_topic_names_and_types()
+            topics = [t[0] for t in topic_list]
+
+            t1 = "/turtle1/cmd_vel" in topics
+            t2 = "/turtle2/cmd_vel" in topics
+
+            if t1 and t2:
+                self.get_logger().info("Both turtle1 and turtle2 detected!")
+                break
+            else:
+                self.get_logger().info("Waiting for both turtles to be available...")
+                time.sleep(2)
+
+    def choose_turtle(self):
+        while True:
+            print("\nChoose turtle1 or turtle2 [1,2]: ")
+            choice = input()
+
+            if choice == "1":
+                print("TURTLE 1!")
+                return self.pub_t1
+
+            elif choice == "2":
+                print("TURTLE 2!")
+                return self.pub_t2
+
+            else:
+                print("Invalid input")
+ 
 
     def user_loop(self):
         
         while rclpy.ok():
-            print("\n select velocity:")
+            print("\n Select velocity:")
 
             try:
                 lin = float(input("Linear velocity (x): "))
                 ang = float(input("Angolar Velocity (z): "))
             except:
-                print("error")
+                print("Invalid input")
                 continue
 
             msg = Twist()
@@ -31,17 +70,13 @@ class UINode(Node):
             msg.angular.z = ang
 
             print("Start")
+            self.selected_pub.publish(msg)
 
-            start = time.time()
-            while time.time() - start < 1.0:
-                self.pub_t1.publish(msg)
-                time.sleep(0.1)
+            time.sleep(1)
 
-            #stop 
             stop_msg = Twist()
-            self.pub_t1.publish(stop_msg)
-
-            print("stop")
+            print("Stop")
+            self.selected_pub.publish(stop_msg)
 
 def main(args=None):
     rclpy.init(args=args)
